b02637c6d634c6ea5d8d797e5c932d9576cd46d9
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 05:31:34 -0600

    profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,16 +3,16 @@ export default function Page() {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
                 <img src="https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                <h1 className="text-5xl font-bold">Your Profile</h1>
-                </div>
             <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 px-30">
+                <div className="justify-center">
+                    <h1 className="text-5xl font-bold justify-center">Your Profile</h1>
+                </div>
                 <form className="card-body">
                     <div className="form-control">
                     <label className="label">
-                        <span className="label-text text-2xl">Username</span>
+                        <span className="label-text text-2xl">Name</span>
                     </label>
-                    <input type="email" placeholder="username" className="input input-bordered" required />
+                    <input type="email" placeholder="name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                     <label className="label">
