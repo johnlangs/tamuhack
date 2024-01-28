cc4c233edfce2b33c623483830ee8695e6da16be
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 01:07:38 -0600

    drop down bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 export default function Navbar() {
     return (
         <div className="navbar bg-base-300">
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </button>
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>About</a></li>
+                        <li><a>smthsmth</a></li>
+                    </ul>
+                </div>
             </div>
             <div className="flex-1">
                 <a href="/" className="btn btn-ghost text-xl">Home</a>
