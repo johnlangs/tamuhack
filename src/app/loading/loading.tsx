ac0f63b47d8abd1cc928d505fcbcd3f0ae6aa04c
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 06:33:44 -0600

    hi

---
+++ src/app/loading/loading.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <span className="loading loading-dots loading-lg"></span>
+            <p className="text-50 justify-center">Loading......</p>
+        </div>
+    );
+}
