07de39a865519d0649f49b07671148edf9c01761
james.hsu <james.hsu.sf@gmail.com>
2024-01-28 01:55:41 -0600

    commit

--- src/app/howdoesitwork/page.tsx
+++ src/app/howdoesitwork/page.tsx
@@ -1,6 +1,14 @@
 export default function Page() {
     return (
-        <>
-        </>
+        <div className="hero min-h-screen" style={{backgroundImage: 'url(/temple.svg)'}}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md mb-20">
+                    <h1 className="mb-8 text-6xl font-bold">Your Privacy Starts Here. </h1>
+                    <p className="mb-6">Your Privacy is important, so is protecting it. Learn more about how to protect yourself in the digital world. </p>
+                    <a href={"/checkyourself"}><button className="btn btn-primary">Get Started</button></a>
+                </div>
+            </div>
+      </div>
     )
 }
