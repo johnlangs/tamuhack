09d61af2885d44ed7606b71e3c853da57e8f632f
james.hsu <james.hsu.sf@gmail.com>
2024-01-27 23:29:06 -0600

    button redirect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,14 @@
+import { redirect } from "next/navigation";
+
 export default function Hero() {
     return (
         <div className="hero min-h-screen" style={{backgroundImage: 'url(/temple.svg)'}}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="mb-5 text-5xl font-bold">Your Privacy Starts Here.</h1>
+                    <p className="mb-5">Your Privacy Starts Here.</p>
+                    <a href = "/login" >< button className="btn btn-primary">Get Started</button> </a>
                 </div>
             </div>
       </div>
