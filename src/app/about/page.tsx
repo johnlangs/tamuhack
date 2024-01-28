9558e813a741876df839e8bb2a0fc78f2b1a369e
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 07:39:00 -0600

    fixed errors, <p> did not want to have <div> inside

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
-        <div className="px-10 pt-20 pb-300 h-screen" style={{ backgroundImage: 'url(/temple.svg)' }}>
-        <div className="hero min-h-screen bg-base-200">
+        <div className="px-30 pt-20 pb-30 h-screen" style={{ backgroundImage: 'url(/temple.svg)' }}>
+        <div className="hero min-h-screen bg-base-100">
             <div className="hero-content flex-col lg:flex-row-reverse px-10">
                 <div>
                     <h1 className="text-5xl font-bold">About This Site</h1>
@@ -15,39 +15,38 @@ export default function Page() {
                         impersonations. We hope to bring awareness and security in the face of evolving online threats.
                         Check out the "How Does it Work?" page next.
                     </p>
-                    <p>
-                        <div className="images flex justify-between pt-1">
-                                <div className="photo">
-                                    <div className="w-55 rounded-xl">
-                                        <img
-                                            src="https://exactitconsulting.com/wp-content/uploads/2017/12/Exact-IT-Security-Comes-In-Two-Parts-Don%E2%80%99t-Neglect-The-Physical-Half-256x256.jpg"
-                                            alt="photo"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="photo">
-                                    <div className="w-55 rounded-xl">
-                                        <img
-                                            src="https://exactitconsulting.com/wp-content/uploads/2017/08/Exact-IT-Are-You-Prepared-For-Incoming-Threats-To-Your-Systems-Category-Security-256x256.jpg"
-                                            alt="photo"
-                                        />
-                                    </div>
-                                </div>
-                                
-                                <div className="photo">
-                                    <div className="w-55 rounded-xl">
-                                        <img
-                                            src="https://i0.wp.com/securityaffairs.co/wordpress/wp-content/uploads/2012/01/KeyCard.gif?resize=256%2C256"
-                                            alt="photo"
-                                        />
-                                    </div>
+                    <p></p>
+                    <div className="images flex justify-between pt-1">
+                            <div className="photo">
+                                <div className="w-55 rounded-xl">
+                                    <img
+                                        src="https://exactitconsulting.com/wp-content/uploads/2017/12/Exact-IT-Security-Comes-In-Two-Parts-Don%E2%80%99t-Neglect-The-Physical-Half-256x256.jpg"
+                                        alt="photo"
+                                    />
                                 </div>
                             </div>
-                        
-                    </p>
+                            <div className="photo">
+                                <div className="w-55 rounded-xl">
+                                    <img
+                                        src="https://exactitconsulting.com/wp-content/uploads/2017/08/Exact-IT-Are-You-Prepared-For-Incoming-Threats-To-Your-Systems-Category-Security-256x256.jpg"
+                                        alt="photo"
+                                    />
+                                </div>
+                            </div>
+                            
+                            <div className="photo">
+                                <div className="w-55 rounded-xl">
+                                    <img
+                                        src="https://i0.wp.com/securityaffairs.co/wordpress/wp-content/uploads/2012/01/KeyCard.gif?resize=256%2C256"
+                                        alt="photo"
+                                    />
+                                </div>
+                            </div>
                     </div>
+                    
                 </div>
             </div>
+        </div>
         </div>
 
     );
