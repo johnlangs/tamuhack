d301e8e016695492ad12015c5b0f3cf9fd5def8f
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 03:58:00 -0600

    profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,28 @@
 export default function Page() {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="avatar justify-center g-10">
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src="https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <div className="w-20 rounded-full">
-                        <img src="https://p7.hiclipart.com/preview/282/256/961/user-profile-avatar-computer-icons-google-account.jpg" />
-                    </div>
-                </div>
-                <div className="mb-5 flex text-400">
-                        <p>Last </p>
+                <h1 className="text-3xl font-bold">Profile</h1>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Your name</span>
+                        </label>
+                        <input type="email" placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="password" placeholder="email" className="input input-bordered" required />
+                        </div>
+                    </form>
                 </div>
             </div>
-            
+        </div>
         </div>
     );
 }
