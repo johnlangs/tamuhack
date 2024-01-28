d965f2069ab58b5420f0ce0100bbfb3ef562f169
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 02:12:45 -0600

    about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,18 @@
 export default function Page() {
     return (
-        <main className="p-2 h-screen">
+        <main className="p-10 h-screen">
             <div className="flex items-center justify-center">
                 <h1 className="mb-100 text-5xl font-bold py={100}">ALL YOU NEED TO KNOW</h1>
             </div>
-            <div className="flex items-center justify-center p-5">
-                <p className="mb-5 text-200"> With the modern technology moving on the widespread dissemination of personal identification information such as
-                birthdays, personal hobbies, family connections, occupation, and so on. 
+            <div className="flex items-center justify-center px-5000 pt-5">
+                <p className="mb-5 text-xl"> The extensive distribution of personally identifiable information includes 
+                birthdays, interests, family, connections, and professional roles, across multiple social media platforms 
+                and the internet. This information has provided a ground for threat actors to fertilize and orchestrate
+                various ways to attack and impersonate individuals without their knowledge. In response to this challenge, this 
+                website is created to serve the purpose of developing an innovative application, methodology, or tool. 
+                The primary objective is to proactively detect potential phishing attempts and guide users to explore 
+                their options for open-source intelligence that could be employed for impersonation purposes.
+                This website aims to boost user awareness and security in the face of evolving online threats. 
                 </p>
             </div>
         </main>
