d3cd2eb846058092a625064e1acad950e95b2c42
james.hsu <james.hsu.sf@gmail.com>
2024-01-28 00:22:54 -0600

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Login Page",
+  title: "Create App",
   description: "Transition to Login Page",
 };
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ export default function Hero() {
         <div className="hero min-h-screen" style={{backgroundImage: 'url(/temple.svg)'}}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-lg">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Your Privacy Starts Here. </h1>
                     <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <a href={"/checkyourself"}><button className="btn btn-primary">Get Started</button></a>
                 </div>
