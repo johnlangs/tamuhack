e33d3034c9816b52fc35391e0a6aa2fc85ee1691
james.hsu <james.hsu.sf@gmail.com>
2024-01-28 06:28:28 -0600

    yes 3

--- src/app/howdoesitwork/page.tsx
+++ src/app/howdoesitwork/page.tsx
@@ -3,9 +3,14 @@ export default function Page() {
         <div className="hero min-h-screen" style={{backgroundImage: 'url(https://img.freepik.com/free-vector/gradient-technology-futuristic-background_23-2149115304.jpg)'}}>
             <div className="hero-overlay bg-opacity-65"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md mb-80">
-                    <h1 className="mb-8 text-5xl font-bold pb-10">How Does it Work? </h1>
-                    <p className="mb-6"> type type type </p>
+                <div className="max-w-md mb-36">
+                    <h1 className="mb-8 text-5xl font-bold pb-2">How Does it Work? </h1>
+                    <p className="mb-2 text-xl"> PrivacyNow incorporates the help of multiple third-party API's to check 
+                    and verify whether your information provided are exposed on the internet on unsafe sites or 
+                    in public all at once. Our servers do not store any of your information beyond your visit to the site 
+                    and all information provided to third-party API's are secure and encrypted. For your protection,
+                    these data are sent to each individual third-party API's anonymously with no correlating association to 
+                    other information provided. </p>
                 </div>
             </div>
       </div>
