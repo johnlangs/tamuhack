6d493afc225a2e43983e7a784ed5a0c02e3c244d
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 01:42:31 -0600

    about us page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <main className="p-2 h-screen">
+            <div className="flex items-center justify-center">
+                <h1 className="mb-100 text-5xl font-bold py={100}">ALL YOU NEED TO KNOW</h1>
+            </div>
+            <div className="flex items-center justify-center p-10">
+                <p className="mb-10 text-25"> This website is designed for people to have </p>
+            </div>
+        </main>
+    );
+}
