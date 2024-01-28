91bd50f8f4aa731ce09b49ebecef9f3a49d2bf2f
james.hsu <james.hsu.sf@gmail.com>
2024-01-28 03:43:08 -0600

    commit again

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,22 @@
 export default function Page() {
     return (
-        <div className="flex items-center justify-center h-screen">
+
+        <div className="flex flex-col gap-4 w-52">
+         <div className="flex gap-4 items-center">
+    <div className="skeleton w-16 h-16 rounded-full shrink-0">
+    <img src="https://p7.hiclipart.com/preview/282/256/961/user-profile-avatar-computer-icons-google-account.jpg" />
+    </div>
+    <div className="flex flex-col gap-4">
+      <div className="skeleton h-4 w-20"></div>
+      <div className="skeleton h-4 w-28"></div>
+    </div>
+  </div>
+  <div className="skeleton h-32 w-full"></div>
+</div> 
+    );
+}
+
+{/* <div className="flex items-center justify-center h-screen">
             <div className="avatar justify-center g-10">
                 <div>
                     <div className="w-20 rounded-full">
@@ -12,6 +28,4 @@ export default function Page() {
                 </div>
             </div>
             
-        </div>
-    );
-}
+        </div> */}
