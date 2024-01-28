d21c0ceca94f43a9e25dbd19ae8b381941ee7872
james.hsu <james.hsu.sf@gmail.com>
2024-01-28 03:16:17 -0600

    hi

--- src/app/howdoesitwork/page.tsx
+++ src/app/howdoesitwork/page.tsx
@@ -1,12 +1,11 @@
 export default function Page() {
     return (
-        <div className="hero min-h-screen" style={{backgroundImage: 'url(/temple.svg)'}}>
+        <div className="hero min-h-screen" style={{backgroundImage: 'https://img.freepik.com/free-vector/gradient-technology-futuristic-background_23-2149115304.jpg'}}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md mb-20">
-                    <h1 className="mb-8 text-6xl font-bold">Your Privacy Starts Here. </h1>
-                    <p className="mb-6">Your Privacy is important, so is protecting it. Learn more about how to protect yourself in the digital world. </p>
-                    <a href={"/checkyourself"}><button className="btn btn-primary">Get Started</button></a>
+                    <h1 className="mb-8 text-4xl font-bold">How Does it Work? </h1>
+                    <p className="mb-6"> type type type </p>
                 </div>
             </div>
       </div>
