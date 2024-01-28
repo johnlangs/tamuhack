a895d685ae4575b36264b8c2a97b8ccb66fe85a2
John Langs <me@johnlangs.dev>
2024-01-28 04:17:12 -0600

    add password checker

--- src/components/OsintForm.tsx
+++ src/components/OsintForm.tsx
@@ -1,4 +1,16 @@
+'use client'
+
+import { passwordStrength } from 'check-password-strength'
+import { ChangeEvent, useState } from 'react';
+
 export default function OsintForm() {
+    const [passStr, setPassStr] = useState(-1)
+
+    const checkPassword = (e: ChangeEvent<HTMLInputElement>) => {
+        const pass = e.target.value
+        setPassStr(passwordStrength(pass).id)
+    }
+
     return (
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form className="card-body">
@@ -6,23 +18,32 @@ export default function OsintForm() {
                 <label className="label">
                     <span className= "font-bold label-text">Email</span>
                 </label>
-                <input name="email" type="email" placeholder= "johnnyappleseed@gmail.com" className="input input-bordered" required />
+                <input name="email" type="email" placeholder= "johnnyappleseed@gmail.com" className="input input-bordered" />
                 </div>
                 <div className="form-control">
                 <label className="label">
                     <span className="font-bold label-text">Phone Number</span>
                 </label>
-                <input name="phome_number" type="text" placeholder="123-456-7890" className="input input-bordered" required />
+                <input name="phome_number" type="text" placeholder="123-456-7890" className="input input-bordered" />
                 </div>
                 <div className="form-control">
                 <label className="label">
                     <span className="font-bold label-text">Address</span>
                 </label>
-                <input name="address" type="text" placeholder="1234 Flower Ln..." className="input input-bordered" required />
+                <input name="address" type="text" placeholder="1234 Flower Ln..." className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                <label className="label">
+                    <span className="font-bold label-text">Password</span>
+                </label>
+                <input name="password" type="password" placeholder="Password" className="input input-bordered" onChange={checkPassword}/>
+                <label className="label">
+                {passStr == 0 ? <p className="text-red-600">Password Too Weak</p> : passStr == 1 ? <p className='text-orange-600'>Weak Password</p> : passStr == 2 ? <p className='text-yellow-300'>Medium Password</p> : passStr == 3 ? <p className='text-lime-500'>Strong Password</p> : <></>}
+                </label>
+                </div>
                 <label className="label">
                     <a href= "/howdoesitwork" className="label-text-alt link link-hover">How does this work?</a>
                 </label>
-                </div>
                 <div className="form-control mt-6">
                 <button type="submit" className="btn btn-primary">Check</button>
                 </div>
