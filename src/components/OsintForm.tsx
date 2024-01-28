3ce49b2d8ee9f4a71849b34e5ac7e0f7699422f4
james.hsu <james.hsu.sf@gmail.com>
2024-01-28 01:02:24 -0600

    hello

--- src/components/OsintForm.tsx
+++ src/components/OsintForm.tsx
@@ -12,10 +12,7 @@ export default function OsintForm() {
                 <label className="label">
                     <span className="label-text">Phone Number</span>
                 </label>
-                <input name="phome_number" type="text" placeholder="1234567890" className="input input-bordered" required />
-                <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">How does this work?</a>
-                </label>
+                <input name="phome_number" type="text" placeholder="123-456-7890" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                 <label className="label">
@@ -23,7 +20,7 @@ export default function OsintForm() {
                 </label>
                 <input name="address" type="text" placeholder="Address" className="input input-bordered" required />
                 <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">How does this work?</a>
+                    <a href= "/howdoesitwork" className="label-text-alt link link-hover">How does this work?</a>
                 </label>
                 </div>
                 <div className="form-control mt-6">
