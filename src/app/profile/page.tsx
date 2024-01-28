bc91f56604144f2265e7b13460dd245a0b0956c7
yuexin zhang <teresayuexinzhang@gmail.com>
2024-01-28 04:22:38 -0600

    profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,43 +1,31 @@
 export default function Page() {
     return (
-
-        <div className="flex flex-col gap-4 w-52">
-         <div className="flex gap-4 items-center">
-    <div className="skeleton w-16 h-16 rounded-full shrink-0">
-    <img src="https://p7.hiclipart.com/preview/282/256/961/user-profile-avatar-computer-icons-google-account.jpg" />
-    </div>
-    <div className="flex flex-col gap-4">
-      <div className="skeleton h-4 w-20"></div>
-      <div className="skeleton h-4 w-28"></div>
-    </div>
-  </div>
-  <div className="skeleton h-32 w-full"></div>
-</div> 
-    );
-}
-
-{/* <div className="flex items-center justify-center h-screen">
-            <div className="avatar justify-center g-10">
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src="https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                <h1 className="text-3xl font-bold">Profile</h1>
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Your name</span>
-                        </label>
-                        <input type="email" placeholder="name" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="password" placeholder="email" className="input input-bordered" required />
-                        </div>
-                    </form>
+                <h1 className="text-5xl font-bold">Your Profile</h1>
                 </div>
+            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 px-30">
+                <form className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text text-2xl">Username</span>
+                    </label>
+                    <input type="email" placeholder="username" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text text-2xl">Email</span>
+                    </label>
+                    <input type="password" placeholder="email" className="input input-bordered" required />
+                    </div>
+                </form>
             </div>
+            </div>
+
             
         </div>
+
     );
-}
+    }
